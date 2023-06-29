16e5d3e8160b42b30af805e25a9ac83675b1bebd
Muhammadjon <shamsulloevmuhammadjon@gmail.com>
2023-06-29 14:09:35 +0500

    Handle games with no image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from '../hooks/useGames';
 import { Card, Image, CardBody, Heading, Text, HStack } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
+import noImage from '../../src/assets/no-image-placeholder.webp';
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card width="200px" borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={game.background_image || noImage} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
